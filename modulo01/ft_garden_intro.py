10123ea35c81fbcc7e5d9f6ca7729ce8b51203bf
rdelicado <rdelicad@gmail.com>
2026-08-24 18:28:37 +0200

    actualizacion

--- modulo01/ft_garden_intro.py
+++ modulo01/ft_garden_intro.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-    print("== Welcome to My Garden ==")
     plant = "Rose"
     height = 25
     age = 30
     print(
+        "== Welcome to My Garden ==\n"
         f"Plant: {plant.capitalize()}\n"
         f"Height: {height}cm "
         f"Age: {age} days\n"
