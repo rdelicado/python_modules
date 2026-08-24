88d8ed6a765afcfa8c79077a9778d5d506ce6219
rdelicado <rdelicad@gmail.com>
2026-08-24 18:26:21 +0200

    actualizacion

--- modulo01/ft_garden_intro.py
+++ modulo01/ft_garden_intro.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     plant = "Rose"
     height = 25
     age = 30
-    print(f"Plant: {plant.capitalize()}")
-    print(f"Height: {height}cm")
-    print(f"Age: {age} days")
-    print("\n== End of Program ==")
+    print(
+        f"Plant: {plant.capitalize()}\n"
+        f"Height: {height}cm"
+        f"Age: {age} days\n"
+        "== End of Program =="
+    )
